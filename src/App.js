3a0153daa4af3d42f38babe4d89374ef67a2944d
oucar1 <oucar1@ivytech.edu>
2025-03-25 15:36:43 -0400

    Initial commit - React Intro 257

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+function Greeting(props) {
+  return (
+    <h1>
+      Hello {props.name} here is your address: {props.address}
+    </h1>
+  );
+}
+
+function Goodbye({ name }) {
+  return <h1>Goodbye {name}</h1>;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Greeting name="Ozgur" address="15480 Royal Grove Court" />
+      <Greeting name="Utku" address="9542 Carlyle Ct" />
+      <Goodbye name="Merve" />
+    </>
   );
 }
 
